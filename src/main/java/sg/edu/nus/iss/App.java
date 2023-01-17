3ee38d5db4798fe4ffe1c6babcb7fc1772586e6a
shiyun-ho <serene.hoshiyun@gmail.com>
2023-01-17 11:02:51 +0800

    Basic Threading Example

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,17 +1,31 @@
 package sg.edu.nus.iss;
 
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Thread thread1 = new Thread(new Runnable() {
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            //specify the code we want to orun
+            for (int i = 0; i<20; i++){
+                System.out.println(Thread.currentThread().getName()
+                + "\tRunnable ..." + i);
+            }
+            }
+       });
+       thread1.start(); 
+
+       MyRunnableImplementation mRI = new MyRunnableImplementation();
+       Thread thread2 = new Thread(mRI);
+       thread2.start();
+
+       Thread thread3 = new Thread(mRI); 
+       thread3.start();
+       }; 
+      
     }
-}
+
---
+++ src/main/java/sg/edu/nus/iss/MyRunnableImplementation.java
@@ -0,0 +1,22 @@
+package sg.edu.nus.iss;
+
+public class MyRunnableImplementation implements Runnable {
+
+    private String taskName; 
+
+    public MyRunnableImplementation(String taskName){
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        for(int i = 0; i<5; i++){
+            System.out.println(Thread.currentThread().getName()
+            + "\t runnable..." + i);
+        }
+    }
+    
+
+
+}
